e7d859fdc2f9a43e1f5b0c1708d9e3470ed523ae
3alifaisal <alifaisalawada000@gmail.com>
2024-07-19 08:57:25 +0200

    use of Context
    learnt the basic syntax to using Context in React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { HoroscopeContext } from "./context/HoroscopeContext";
+import SideCard from "./components/SideCard";
 
 const Root = () => {
   return (
+    <HoroscopeContext.Provider value={{ sign: "leo" }}>
       <App />
+    </HoroscopeContext.Provider>
   );
 };
 
@@ -13,5 +17,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
